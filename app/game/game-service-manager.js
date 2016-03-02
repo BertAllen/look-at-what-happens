f61397a524d5eae5146ed3fcfe8f099fdc56650a
Bert <Bert>
2016-03-02 15:25:45 -0700

    still working on trait exclusion

--- app/game/game-service-manager.js
+++ app/game/game-service-manager.js
@@ -1,5 +1,5 @@
 app.service('GameManager', function(CharactersService){
-	
+	var traitCost =2;
 	var _computerChoice;
 	var _game = {
 		characters: CharactersService.getCharacters(),
@@ -26,24 +26,42 @@ app.service('GameManager', function(CharactersService){
 	
 	this.checkProperty = function(prop){
 		if(gameOver()){ return }
+		var hasProp = false;
+		var found = false;
         
+        if(_game.guesses + traitCost >10){
+            _game.message = 'You do not have enough guess credits left to eliminate that trait.'
+            return;
         /**  CAN GUESS
          * Check if the traitCost is greater than remaning guesses
-         * totalGuesses = 10; guesses starts at 0 and should be for each guess
+         * totalGuesses = 10; guesses starts at 0 and should be >incremented< for each guess
          * the traitCost starts at 2 and goes up by one for each propertyCheck
          * Dont forget to add the traitCost to the _game.guesses
          * setting _game.message will provide the user with feedback
          */
+        } else {
+            _game.guesses += traitCost;
+            traitCost ++;
+            prop.used = true;
+        } //end of if statement --^
         
         /**  _COMPUTERCHOICE has TRAIT
          * _computerChocie.traits === [String, String]
          * check if _computerChoice.traits has the selected prop.name if so set hasProp = true;
          * also set prop.used = true to disable the same trait check
-         */
-		
-		var hasProp = false;
-		var found = false;
+         *///here is this snippet --v
+        for(var i=0; i< _computerChoice.traits.length; i++){
+        if(_computerChoice.traits[i] === prop){
+            hasProp = true;
+        }
+        }
         
+        // v--from here to the end of the function still needs to be rethought/redone ..............
+         _game.characters.forEach(function(character){
+			character.traits.forEach(function(trait){
+				_game.propertyList[trait]===prop;
+			})
+		});
         /** EACH CHARACTER HAS TRAIT
          * now check each _game.characters individually
          * if the character.traits has prop.name set 
@@ -58,15 +76,29 @@ app.service('GameManager', function(CharactersService){
          */
 	}
 	
-	function reset(){
+	function reset(){ //DONE!!
+    _game.guesses =0;
+    traitCost =2;
+    
         /**
          * Reset all of the values on _game
          * each character on _game.characters should set to 
          * character.possible = true
+         * //v-- loop one
+         */
+        for(var i=0; i<_game.characters.length; i++){
+            _game.characters[i].possible =true;
+        }
+        /*
          * all of the traits in  _game.propertyList <--- its an {}
          * should be set to 
          * _game.propertyList[trait].used = false
-         */
+         *///v-- loop two
+        _game.characters.forEach(function(character){
+			character.traits.forEach(function(trait){
+				_game.propertyList[trait].used = false;
+			})
+		});
 	}
 	
 	function gameOver() {
@@ -77,13 +109,20 @@ app.service('GameManager', function(CharactersService){
          * set _game.computerChoice = _computerChoice
          * return true
          */
+        if(_game.guesses <= 10){
+            return false;
+        } else if(amazing-logic-here){
+            
+        }
 	}
 	
-	function setRandomChoice(){
+	function setRandomChoice(){ //not sure if done correctly yet
         /**
          * This function should get a random index between 0 - _game.characters.length
          * then set _computerChoice to the object at the randI index
          */
+        var whoPick = Math.floor(Math.random() * _game.characters.length);
+        _computerChoice = _game.characters[whoPick];
 	}
 	
 })
